adde80f48fe7df5bef919409ad8d424b99ee72d7
Kayce Furlong <kayce_lf@live.com>
2019-06-27 19:17:59 -0700

    starting game

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    //declare variables and set variables
+    var timer = 25,
+        correct = 0,
+        incorrect = 0,
+        unanswered = 0,
+        currentQuestion = 0,
+        myTimer;
+
